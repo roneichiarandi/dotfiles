b15890f3aa8c0a096c129e70f393b56d05c3d4e5
Ronei Chiarandi <rchiarandi@uolinc.com>
2016-06-03 09:09:27 -0300

    Adding setup configuration

--- setup.sh
+++ setup.sh
@@ -2,18 +2,27 @@
 
 DOTFILES_ROOT=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
-DOTFILES= # verificar arquivos da pasta
+DOTFILES=($(find ${DOTFILES_ROOT} -maxdepth 1 -type f -printf '%f '))
 
 for dotfile in "${DOTFILES[@]}"
 do
-  if [ -h ~/."${dotfile}" ]
+  if [[ "${dotfile}" != "setup.sh" ]] && [[ "${dotfile}" != "README.md" ]];
   then
-    printf "~/."${dotfile}" exists\n"
-  else
-    ln -s "${DOTFILES_ROOT}"/"${dotfile}" ~/."${dotfile}"
-    printf "~/."${dotfile}" linked\n"
+    if [ -f ~/."${dotfile}" ]
+    then
+      printf "~/.${dotfile} exists\n"
+    else
+      ln -s "${DOTFILES_ROOT}"/"${dotfile}" ~/."${dotfile}"
+      printf "~/.${dotfile} linked\n"
+    fi
   fi
 done
+
+if [[ -d ~/.oh-my-zsh ]]; then
+  rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
+  ln -s "${DOTFILES_ROOT}"/"oh-my-zsh/themes/agnoster.zsh-theme" ~/.oh-my-zsh/themes/agnoster.zsh-theme
+  printf "\nAgnoster modified theme linked\n\n"
+fi
 
 printf "%s installation end\n" $DOTFILES_ROOT
 
