fdc7d5b945ac34094521269be4c821fbc2de92dd
Ronei Chiarandi <rchiarandi@uolinc.com>
2025-06-05 14:34:13 -0300

    setup bin

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 DOTFILES_ROOT=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 DOTFILES=($(find ${DOTFILES_ROOT} -mindepth 1 -maxdepth 1 -not -path '*/.*' -type f -exec printf "%s\n" {} + |  awk -F/ '{print $NF}'))
+
+DOTFILES_BIN_PATH=${DOTFILES_ROOT}/bin/
+
+BIN_PATH=/usr/local/bin/
 
 for dotfile in "${DOTFILES[@]}"
 do
@@ -28,5 +32,22 @@ if [[ -d ~/.oh-my-zsh ]]; then
   fi
 fi
 
-printf "%s installation end\n" $DOTFILES_ROOT
+
+printf "Setup dotfiles (alias)\n"
+for bin in "${DOTFILES_BIN_PATH}"*;
+do
+  file=$(basename $bin)
+  if [[ "${file}" != "README.md" ]]
+  then
+    if [ -f ${BIN_PATH}"${bin}" ]
+    then
+      printf ${bin} exists\n
+    else
+      ln -s ${bin} ${BIN_PATH}${file}
+      printf "${file} linked\n"
+    fi
+  fi
+done
+
+printf "%s installation end\n"
 
